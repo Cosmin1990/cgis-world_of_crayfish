c7c412bb3f4e852cce782da56adba094363c6b2c
victorbacu <victorbacu@gmail.com>
2025-11-08 10:06:28 +0200

    Fix

--- woc-portal/src/index.tsx
+++ woc-portal/src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/woc/">
+    <BrowserRouter basename="/woc">
       <App />
     </BrowserRouter>
   </React.StrictMode>
