1a045dabdd84fb4aff8b09ef0b5f6da6310b380d
victorbacu <victorbacu@gmail.com>
2026-03-13 11:43:39 +0200

    Update

--- woc-server/ServerApp.py
+++ woc-server/ServerApp.py
@@ -14,11 +14,7 @@ ServerApp = Flask(__name__)
 
 # For development purposes only!!!!!!!
 CORS(ServerApp,
-     resources={
-         r"/woc/.*": {
-             "origins": ["https://world.crayfish.ro"]
-         }
-     }
+     origins=["https://world.crayfish.ro"],
      )
 
 
